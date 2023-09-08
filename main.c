7c2061eb84dfc2b58b225cb8a516815c45ba81fc
isabelge <isabel.geissmann@unibas.ch>
2023-09-08 14:33:21 +0200

    add solution for variablen

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    printf("Variablen");
+    printf("Aufgabe 2");
+
+    int a, b = 6, c, d = 1;
+
+    a = b / 2;
+    printf("a = %d\n", a);
+    c = b % 2;
+    printf("c = %d\n", c);
+    b = 1 - --b;
+    printf("b = %d\n", b);
+    a = --b + d++;
+    printf("a = %d\n", a);
+
+    a = 0;
+    b = 2;
+    c = 3;
+    d = 4;
+
+    a = (b + 2) * 2 * c + 1;
+    printf("a = %d\n", a);
+    a = ++b * d++ * ++c;
+    printf("a = %d\n", a);
+    return 0;
+
+}
